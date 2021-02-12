f1556beb97ff92a66682d915462582e1edc27187
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2021-02-12 13:53:02 -0300

    Visual fixes

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,8 +5,13 @@
             <p><a href="https://www.phbit.com.br" target="_blank">www.phbit.com.br</a>
             <br><b-badge variant="primary">website</b-badge> <b-badge variant="success">no ar</b-badge> <b-badge variant="danger">vuejs</b-badge></p>
 
-            <div class="card-preview">
-                <a href="https://www.phbit.com.br" target="_blank"><b-img src="images/portfolio/phbit-website.png" class="card-width-image" /></a>
+            <div class="portfolio-link text-center text-muted small">
+                <div class="card-preview">
+                    <a href="https://www.phbit.com.br" target="_blank">
+                        <b-img src="images/portfolio/phbit-website.png" class="card-width-image" />
+                    </a>
+                </div>
+                Click/touch to browse
             </div>
         </b-col>
 
@@ -15,8 +20,13 @@
             <p><a href="https://www.nexseg.com.br" target="_blank">www.nexseg.com.br</a>
             <br><b-badge variant="primary">website</b-badge> <b-badge variant="success">no ar</b-badge> <b-badge variant="danger">vuejs</b-badge></p>
 
-            <div class="card-preview">
-                <a href="https://www.nexseg.com.br" target="_blank"><b-img src="images/portfolio/nexseg-website.jpg" class="card-width-image" /></a>
+            <div class="portfolio-link text-center text-muted small">
+                <div class="card-preview">
+                    <a href="https://www.nexseg.com.br" target="_blank">
+                        <b-img src="images/portfolio/nexseg-website.jpg" class="card-width-image" />
+                    </a>
+                </div>
+                Click/touch to browse
             </div>
         </b-col>
 
@@ -25,7 +35,10 @@
             <p><a href="https://app.nexseg.com.br">app.nexseg.com.br</a>
             <br><b-badge variant="warning">app</b-badge> <b-badge variant="success">no ar</b-badge> <b-badge variant="danger">vuejs</b-badge></p>
 
-            <gallery :images="nexsegAppImages" class="card-gallery" />
+            <div class="portfolio-link text-center text-muted small">
+                <gallery :images="nexsegAppImages" class="card-gallery" />
+                Click/touch to enlarge
+            </div>
         </b-col>
 
         <b-col md="6" lg="4" class="px-4 mb-5">
@@ -33,7 +46,10 @@
             <p><a href="https://www.nexsin.com.br">www.nexsin.com.br</a>
             <br><b-badge variant="warning">app</b-badge> <b-badge variant="success">no ar</b-badge> <b-badge variant="light">php</b-badge> <b-badge variant="light">html</b-badge> <b-badge variant="light">js</b-badge> <b-badge variant="light">css</b-badge></p>
 
-            <gallery :images="nexsinAppImages" class="card-gallery" />
+            <div class="portfolio-link text-center text-muted small">
+                <gallery :images="nexsinAppImages" class="card-gallery" />
+                Click/touch to enlarge
+            </div>
         </b-col>
 
         <b-col md="6" lg="4" class="px-4 mb-5">
@@ -41,8 +57,13 @@
             <p><a href="https://www.affianceconsorcios.com.br">www.affianceconsorcios.com.br</a>
             <br><b-badge variant="primary">website</b-badge> <b-badge variant="success">no ar</b-badge> <b-badge variant="light">php</b-badge> <b-badge variant="light">html</b-badge> <b-badge variant="light">js</b-badge> <b-badge variant="light">css</b-badge></p>
 
-            <div class="card-preview">
-                <a href="https://www.affianceconsorcios.com.br"><b-img src="images/portfolio/affiance-consorcios-website.png" class="card-width-image" /></a>
+            <div class="portfolio-link text-center text-muted small">
+                <div class="card-preview">
+                    <a href="https://www.affianceconsorcios.com.br">
+                        <b-img src="images/portfolio/affiance-consorcios-website.png" class="card-width-image" />
+                    </a>
+                </div>
+                Click/touch to browse
             </div>
         </b-col>
 
@@ -51,8 +72,13 @@
             <p><a href="https://www.dataplain.com.br">www.dataplain.com.br</a>
             <br><b-badge variant="primary">website</b-badge> <b-badge variant="success">no ar</b-badge> <b-badge variant="danger">vuejs</b-badge></p>
 
-            <div class="card-preview">
-                <a href="https://www.dataplain.com.br"><b-img src="images/portfolio/dataplain-website.jpg" class="card-width-image" /></a>
+            <div class="portfolio-link text-center text-muted small">
+                <div class="card-preview">
+                    <a href="https://www.dataplain.com.br">
+                        <b-img src="images/portfolio/dataplain-website.jpg" class="card-width-image" />
+                    </a>
+                </div>
+                Click/touch to browse
             </div>
         </b-col>
 
@@ -61,7 +87,10 @@
             <p><a href="https://www.chapeco.com.br">www.chapeco.com.br</a>
             <br><b-badge variant="warning">app</b-badge> <b-badge variant="success">no ar</b-badge> <b-badge variant="light">php</b-badge> <b-badge variant="light">html</b-badge> <b-badge variant="light">js</b-badge> <b-badge variant="light">css</b-badge></p>
 
-            <gallery :images="balcaoEmpregosImages" class="card-gallery" />
+            <div class="portfolio-link text-center text-muted small">
+                <gallery :images="balcaoEmpregosImages" class="card-gallery" />
+                Click/touch to enlarge
+            </div>
         </b-col>
     </b-row>
 </template>
@@ -129,8 +158,12 @@ export default {
 </script>
 
 <style>
+.portfolio-link a {
+    color: var(--light);
+}
 .card-preview {
     height: 240px;
+    margin-bottom: 5px;
     overflow-y: auto;
 }
 .card-width-image {
@@ -139,6 +172,7 @@ export default {
 }
 .card-gallery {
     height: 240px;
+    margin-bottom: 5px;
     overflow: hidden;
     /* object-fit: cover; */
 }
